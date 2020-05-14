ffc85c8069844c876652e424a4b10b87089df125
luisfeliperd <luisfelipeprf@gmail.com>
2020-05-14 01:01:15 -0300

    Update tables.py

--- crossover/dicts/tables.py
+++ crossover/dicts/tables.py
@@ -6,9 +6,10 @@ Created on Tue Apr 14 11:01:23 2020
 """
 
 dfTables = {
-        'collection':['id_instalacao','id','name','description','creation_date','modification_date','url'],
-        'taxonomy':['id','name','description','allow_insert'],
-        'term':['id_taxonomia','id','name','description','url', 'url_imagem', 'parent'],
+        'collection':['id_instalacao','name','description','creation_date','modification_date','url'],
+        'taxonomy':['name','description','allow_insert'],
+        'term':['id','name','description','url', 'url_imagem', 'parent'],
         'inst_col_tax':['id_instalacao','id_colecao','id_taxonomia'],
-        'metadata':['id_instalacao','id_colecao','id','name','description','metadata_type','required','collection_key', 'multiple','display','semantic_uri']
+        'metadata':['id_instalacao','id_colecao','id','name','description','metadata_type','required','collection_key', 'multiple','display','semantic_uri'],
+        'term_tax':['taxonomy_id','term_id']
         }
